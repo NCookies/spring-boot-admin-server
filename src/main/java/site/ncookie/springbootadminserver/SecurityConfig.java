ce8086cee2ca973501716f19c10fac442693227d
NCookies <swstar21c@gmail.com>
2023-07-15 14:49:29 +0900

    SecurityConfig 주석 추가

--- src/main/java/site/ncookie/springbootadminserver/SecurityConfig.java
+++ src/main/java/site/ncookie/springbootadminserver/SecurityConfig.java
@@ -19,20 +19,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 로그인 성공 시 메인페이지로 redirect
         final SavedRequestAwareAuthenticationSuccessHandler loginSuccessHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         loginSuccessHandler.setTargetUrlParameter("redirectTo");
         loginSuccessHandler.setDefaultTargetUrl(this.adminServer.path("/"));
 
         http
                 .authorizeHttpRequests()
+                // 로그인 페이지와 assets 리소스는 누구나 접근할 수 있도록 허용
                 .requestMatchers(
                         "/login",
                         "/assets/*"
                 ).permitAll()
+                // 그 외에는 접근 권한이 필요함
                 .anyRequest().authenticated()
 
                 .and()
+                // 로그인 URL 및 success handler 설정
                 .formLogin().loginPage(this.adminServer.path("/login")).successHandler(loginSuccessHandler)
+                // 로그아웃 URL 설정
                 .and().logout().logoutUrl("/logout")
 
                 .and()
